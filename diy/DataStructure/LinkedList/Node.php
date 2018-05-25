7efd05235a73bbdc42e54e5d4fca69fdfa0b75ef
A. Akbar Hidayat <diqo@dicoding.com>
2018-05-26 03:38:44 +0700

    basic node

---
+++ diy/DataStructure/LinkedList/Node.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Diy\DataStructure\LinkedList;
+
+class Node
+{
+    /**
+     * @var mixed
+     */
+    private $value;
+    /**
+     * @var Node
+     */
+    private $next;
+    /**
+     * @var Node
+     */
+    private $previous;
+
+    public function __construct($value, Node $next, Node $previous)
+    {
+        $this->value = $value;
+        $this->next = $next;
+        $this->previous = $previous;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @return Node
+     */
+    public function getNext(): Node
+    {
+        return $this->next;
+    }
+
+    /**
+     * @return Node
+     */
+    public function getPrevious(): Node
+    {
+        return $this->previous;
+    }
+
+}
