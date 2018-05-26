abb56446bba75a792252e2710c1e9ec37e538569
A. Akbar Hidayat <diqo@dicoding.com>
2018-05-27 05:15:13 +0700

    can find the tail

--- tests/LinkedList/FindANodeTest.php
+++ tests/LinkedList/FindANodeTest.php
@@ -49,4 +49,26 @@ class FindANodeTest extends \PHPUnit\Framework\TestCase
             $doubleLinkedList->find(5)
         );
     }
+
+    /**
+     * @test
+     */
+    public function canFindTheTail(): void
+    {
+        $doubleLinkedList = new DoubleLinkedList();
+        $one = $doubleLinkedList->prepend(1);
+        $four = $doubleLinkedList->append(4);
+        $three = $doubleLinkedList->insertBefore(3, $four);
+        $two = $doubleLinkedList->insertBefore(2, $three);
+        $five = $doubleLinkedList->prepend(5); // head
+        $zero = $doubleLinkedList->insertBefore(0, $one);
+        $hello = $doubleLinkedList->insertBefore('hello', $two);
+        $seven = $doubleLinkedList->insertAfter(7, $hello);
+        $eight = $doubleLinkedList->insertAfter(8, $four);
+
+        $this->assertSame(
+            $eight,
+            $doubleLinkedList->find(8)
+        );
+    }
 }
