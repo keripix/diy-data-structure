ce0ca76817945c160ceb9c9be46441faea32d1b5
A. Akbar Hidayat <diqo@dicoding.com>
2018-05-29 02:50:00 +0700

    using the double linked list

---
+++ benchmarks/doubleLinkedList.php
@@ -0,0 +1,26 @@
+<?php
+
+ini_set('memory_limit', '2G');
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$doubleLinkedList = new \Diy\DataStructure\LinkedList\DoubleLinkedList();
+$start = memory_get_usage();
+
+try {
+    for ($i = 0; $i < 60000; $i++) {
+        $doubleLinkedList->prepend(random_int(0, 10));
+    }
+} catch (Exception $exception) {
+    dump($exception);
+}
+
+$finished = memory_get_usage();
+$delta = $finished - $start;
+
+dump([
+    'Start' => $start,
+    'Finish' => $finished,
+    'Delta' => $delta,
+    'Peak' => memory_get_peak_usage()
+]);
